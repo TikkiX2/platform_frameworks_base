f428fc703eb888773c6151ba61d9a11b34ed87d1
TikkiTikki <radstylemusic@gmail.com>
2020-04-12 07:07:18 -0300

    Testing AncientUI
    by.tikkiX2

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -236,20 +236,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mSystemIconsView = findViewById(R.id.quick_status_bar_system_icons);
         mQuickQsStatusIcons = findViewById(R.id.quick_qs_status_icons);
 
-        mClockDateContainer = findViewById(R.id.clock_date_container);
-        mStatusIconsContainer = findViewById(R.id.status_icons_container);
-        // Make mStatusIconsContainer the same height of mClockDateContainer
-        mClockDateContainer.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                mClockDateContainer.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams)
-                        mStatusIconsContainer.getLayoutParams();
-                lp.height = mClockDateContainer.getHeight();
-                mStatusIconsContainer.setLayoutParams(lp);
-            }
-        });
-
         StatusIconContainer iconContainer = findViewById(R.id.statusIcons);
         iconContainer.setShouldRestrictIcons(false);
         mIconManager = new TintedIconManager(iconContainer);
@@ -401,7 +387,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mHeaderTextContainerView.setLayoutParams(mHeaderTextContainerView.getLayoutParams());*/
 
         int topMargin = resources.getDimensionPixelSize(
-                com.android.internal.R.dimen.quick_qs_offset_height) + (mHeaderImageEnabled ?
+                R.dimen.ancient_minimal_height) + (mHeaderImageEnabled ?
                 resources.getDimensionPixelSize(R.dimen.qs_header_image_offset) : 0);
 
         mSystemIconsView.getLayoutParams().height = topMargin;
@@ -412,7 +398,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
             lp.height = topMargin;
         } else {
             int qsHeight = resources.getDimensionPixelSize(
-                    com.android.internal.R.dimen.quick_qs_total_height);
+                    R.dimen.ancient_minimal_height);
 
             if (mHeaderImageEnabled) {
                 qsHeight += resources.getDimensionPixelSize(R.dimen.qs_header_image_offset);
